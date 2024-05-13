55e56f21aa389c59e77dfdd9986812b7da5b5452
Gokkul <gokkulspartan@gmail.com>
2024-05-13 16:20:22 +0530

    adding_records

--- MyProject/website/views.py
+++ MyProject/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm,AddRecordForm
 from .models import Record
 
 def home(request):
@@ -59,4 +59,20 @@ def delete_record(request, pk):
 		return redirect('home')
 	else:
 		messages.success(request, "You Must Be Logged In To Do That...")
+		return redirect('home')
+
+def add_record(request):
+     return render(request,'add_record.html',{})
+
+def add_record(request):
+	form = AddRecordForm(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			if form.is_valid():
+				add_record = form.save()
+				messages.success(request, "Record Added successfully!")
+				return redirect('home')
+		return render(request, 'add_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged InS")
 		return redirect('home')
